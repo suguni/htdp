2be7486784ceeef789d3278f0f475830366daedc
dajeong <gyul6@hanmail.net>
2011-12-12 19:33:11 +0900

    test

---
+++ dajeong/test.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname test) (read-case-sensitive #t) (teachpacks ((lib "draw.ss" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.ss" "teachpack" "htdp")))))
+(define (hello-world name)
+  (list 'hello-word name))
